98678c4bbe73cf8a1a611aa192ffa836b912032f
Arthur Gusmao <arthur.cabj@hotmail.com>
2023-07-31 20:27:58 -0300

    fix: logic to if

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,5 +1,5 @@
 int ft_isalpha(char c) {
-    while ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
         return 1; 
     }
     return (0);
--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,5 +1,5 @@
 int ft_isdigit(char c) {
-    while (c >= '0' && c <= '9') {
+    if (c >= '0' && c <= '9') {
         return (1); 
     }
     return (0); 
